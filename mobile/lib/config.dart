e9b85fc8d907307535957d8fd8373433342def85
hyacintheaymar <hyacintheaymaryameogo@gmail.com>
2026-06-26 16:06:23 +0000

    chore: point mobile app to production Render backend

--- mobile/lib/config.dart
+++ mobile/lib/config.dart
@@ -8,18 +8,7 @@ class AppConfig {
       return _apiBaseUrlFromEnv;
     }
 
-    if (kIsWeb) {
-      return 'http://127.0.0.1:4000';
-    }
-
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      return 'http://10.0.2.2:4000';
-    }
-
-    if (defaultTargetPlatform == TargetPlatform.iOS) {
-      return 'http://127.0.0.1:4000';
-    }
-
-    return 'http://127.0.0.1:4000';
+    // URL du backend en production sur Render
+    return 'https://fasotransport-v2.onrender.com';
   }
 }
